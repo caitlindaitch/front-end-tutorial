8d78d7315ccbd50071222474a0374e0ef77041e9
Caitlin Daitch <caitlin.daitch@gmail.com>
2019-12-19 20:42:10 -0500

    Start building out AB test input CTA logic

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ var value = Math.random() * 10;
 var int = Math.floor(value);
 var testGroup = int % 2 === 0;
 
-console.log(testGroup)
+let button = document.getElementById("invest-cta");
 
-window.localStorage.setItem('user', JSON.stringify(person));
+if (testGroup) {
+  button.innerHTML = "Invest $500 now";
+} else {
+  button.innerHTML = "Invest now";
+}
+
+
+// window.localStorage.setItem('user', JSON.stringify(person));
