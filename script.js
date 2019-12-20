079a920c69d4330c97351a53eeb47244be21f74d
Caitlin Daitch <caitlin.daitch@gmail.com>
2019-12-19 20:52:23 -0500

    Implement localstorage logic for AB test

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-var value = Math.random() * 10;
-var int = Math.floor(value);
-var testGroup = int % 2 === 0;
+var localStorageValue = "invest-cta";
 
-let button = document.getElementById("invest-cta");
+var storedTestGroup = window.localStorage.getItem(localStorageValue);
+var group;
 
-if (testGroup) {
+if (!storedTestGroup) {
+  var value = Math.random() * 10;
+  var int = Math.floor(value);
+  group = int % 2 === 0;
+  window.localStorage.setItem(localStorageValue, group);
+}
+
+var button = document.getElementById("invest-cta");
+
+if (group === true) {
   button.innerHTML = "Invest $500 now";
 } else {
   button.innerHTML = "Invest now";
 }
 
-
-// window.localStorage.setItem('user', JSON.stringify(person));
+// add this back and the CTA will stay the same
+// window.localStorage.setItem(localStorageValue, group);
+window.localStorage.removeItem(localStorageValue);
